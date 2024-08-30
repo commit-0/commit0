7fe41f3b71d9fd17da1faad07a54f354ce0cbdb0
Wenting Zhao <wentingzhao@outlook.com>
2024-08-30 04:00:53 +0000

    renaming

--- scripts/test_evaluate.sh
+++
@@ -1,8 +0,0 @@
-python run.py \
-    --dataset_name wentingzhao/spec2repo \
-    --split train \
-    --max_workers 2 \
-    --predictions_path 'gold' \
-    --instance_ids spec2repo__tinydb-01 \
-    --run_id validate-gold \
-    --spec_config configs/specs.yml
--- scripts/test_model_output.sh
+++
@@ -1,7 +0,0 @@
-python -m swebench.harness.run_evaluation \
-    --dataset_name spec2repo \
-    --split test \
-    --max_workers 2 \
-    --predictions_path $1 \
-    --instance_ids spec2repo__minitorch-01 \
-    --run_id validate-model-output
