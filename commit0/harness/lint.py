d2c00397d09c640a56211c20dd6ca440298565d2
Wenting Zhao <wentingzhao@outlook.com>
2024-09-19 05:26:55 +0000

    pre-commit and ci

--- commit0/harness/lint.py
+++ commit0/harness/lint.py
@@ -3,8 +3,7 @@ import sys
 from pathlib import Path
 
 
-config = \
-"""repos:
+config = """repos:
 # Standard hooks
 - repo: https://github.com/pre-commit/pre-commit-hooks
   rev: v4.3.0
@@ -32,14 +31,17 @@ def main(base_dir: str, files: list[str]) -> None:
     config_file = Path(".commit0.pre-commit-config.yaml")
     if not config_file.is_file():
         config_file.write_text(config)
-    command = ['pre-commit', 'run', '--config', config_file, '--files'] + files
+    command = ["pre-commit", "run", "--config", config_file, "--files"] + files
     try:
         result = subprocess.run(command, capture_output=True, text=True, check=True)
         print(result.stdout)
         sys.exit(result.returncode)
     except subprocess.CalledProcessError as e:
         raise Exception(f"Pre-commit checks failed\n{e.output}")
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         raise FileNotFoundError("Error: pre-commit command not found. Is it installed?")
     except Exception as e:
         raise Exception(f"An unexpected error occurred: {e}")
+
+
+__all__ = []
