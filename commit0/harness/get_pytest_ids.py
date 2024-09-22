4858144060a3c551dfc798b7580c69fdf1ed2286
Wenting Zhao <wentingzhao@outlook.com>
2024-09-22 01:58:33 +0000

    pre-commit fixes

--- commit0/harness/get_pytest_ids.py
+++ commit0/harness/get_pytest_ids.py
@@ -5,8 +5,10 @@ from typing import List
 def main(repo: str, verbose: int) -> List[str]:
     repo = repo.lower()
     repo = repo.replace(".", "-")
-    with bz2.open(f"commit0/data/test_ids/{repo}.bz2", 'rt') as f:
+    with bz2.open(f"commit0/data/test_ids/{repo}.bz2", "rt") as f:
         out = f.read()
+    if verbose:
+        print(out)
     out = out.split("\n")
     return out
 
