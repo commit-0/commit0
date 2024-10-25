c180a199b3e8d525c9d48117a7870fb068ea86f6
Max Whitton <mmw243@cornell.edu>
2024-10-25 13:23:37 -0400

    new file

--- agent/run_agent.py
+++ agent/run_agent.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 import yaml
 import multiprocessing
 from datasets import load_dataset
@@ -12,6 +13,7 @@ from agent.agent_utils import (
     read_yaml_config,
 )
 from agent.agents import AiderAgents
+from agent.agents import AgentTeams
 from typing import Optional, Type, cast
 from types import TracebackType
 from agent.class_types import AgentConfig
@@ -42,8 +44,8 @@ class DirContext:
     ) -> None:
         os.chdir(self.cwd)
 
-
 def run_agent_for_repo(
+    commit0_config: Any,
     repo_base_dir: str,
     agent_config: AgentConfig,
     example: RepoInstance,
@@ -53,6 +55,44 @@ def run_agent_for_repo(
     backend: str = "modal",
     log_dir: str = str(RUN_AGENT_LOG_DIR.resolve()),
 ) -> None:
+    if agent_config.agent_name == "aider":
+        run_aider_for_repo(
+                            commit0_config["base_dir"],
+                            agent_config,
+                            cast(RepoInstance, example),
+                            update_queue,
+                            branch,
+                            override_previous_changes,
+                            backend,
+                            log_dir,
+                        )
+    elif agent_config.agent_name == "aider_team":
+        run_team_for_repo(
+                            commit0_config["base_dir"],
+                            agent_config,
+                            cast(RepoInstance, example),
+                            update_queue,
+                            branch,
+                            override_previous_changes,
+                            backend,
+                            log_dir,
+                        )
+    else:
+        raise NotImplementedError(
+            f"{agent_config.agent_name} is not implemented; please add your implementations in baselines/agents.py."
+        )
+def run_aider_for_repo(
+    repo_base_dir: str,
+    agent_config: AgentConfig,
+    example: RepoInstance,
+    update_queue: multiprocessing.Queue,
+    branch: str,
+    override_previous_changes: bool = False,
+    backend: str = "modal",
+    log_dir: str = str(RUN_AGENT_LOG_DIR.resolve()),
+) -> None:
+    
+    agent = AiderAgents(agent_config.max_iteration, agent_config.model_name)
     """Run Aider for a given repository."""
     # get repo info
     _, repo_name = example["repo"].split("/")
@@ -74,12 +114,7 @@ def run_agent_for_repo(
             f"{repo_path} is not a git repo. Check if base_dir is correctly specified."
         )
 
-    if agent_config.agent_name == "aider":
-        agent = AiderAgents(agent_config.max_iteration, agent_config.model_name)
-    else:
-        raise NotImplementedError(
-            f"{agent_config.agent_name} is not implemented; please add your implementations in baselines/agents.py."
-        )
+    
 
     # # if branch_name is not provided, create a new branch name based on agent_config
     # if branch is None:
@@ -160,6 +195,7 @@ def run_agent_for_repo(
             update_queue.put(
                 ("start_repo", (original_repo_name, len(target_edit_files)))
             )
+            
             for f in target_edit_files:
                 update_queue.put(("set_current_file", (repo_name, f)))
                 dependencies = import_dependencies[f]
@@ -174,6 +210,132 @@ def run_agent_for_repo(
                         (repo_name, file_name, agent_return.last_cost),
                     )
                 )
+    update_queue.put(("finish_repo", original_repo_name))
+def run_team_for_repo(
+    repo_base_dir: str,
+    agent_config: AgentConfig,
+    example: RepoInstance,
+    update_queue: multiprocessing.Queue,
+    branch: str,
+    override_previous_changes: bool = False,
+    backend: str = "modal",
+    log_dir: str = str(RUN_AGENT_LOG_DIR.resolve()),
+) -> None:
+    """Run Aider for a given repository."""
+    # get repo info
+    _, repo_name = example["repo"].split("/")
+
+    # before starting, display all information to terminal
+    original_repo_name = repo_name
+    update_queue.put(("start_repo", (original_repo_name, 0)))
+
+    # repo_name = repo_name.lower()
+    # repo_name = repo_name.replace(".", "-")
+
+    repo_path = os.path.join(repo_base_dir, repo_name)
+    repo_path = os.path.abspath(repo_path)
+
+    try:
+        local_repo = Repo(repo_path)
+    except Exception:
+        raise Exception(
+            f"{repo_path} is not a git repo. Check if base_dir is correctly specified."
+        )
+
+    manager = AiderAgents(1, agent_config.model_name)
+    coder = AiderAgents(agent_config.max_iteration, agent_config.model_name)
+    
+
+    # # if branch_name is not provided, create a new branch name based on agent_config
+    # if branch is None:
+    #     branch = args2string(agent_config)
+    create_branch(local_repo, branch, example["base_commit"])
+
+    # in cases where the latest commit of branch is not commit 0
+    # set it back to commit 0
+    latest_commit = local_repo.commit(branch)
+    if latest_commit.hexsha != example["base_commit"] and override_previous_changes:
+        local_repo.git.reset("--hard", example["base_commit"])
+
+    target_edit_files, import_dependencies = get_target_edit_files(
+        local_repo,
+        example["src_dir"],
+        example["test"]["test_dir"],
+        str(latest_commit),
+        example["reference_commit"],
+    )
+
+
+    # Call the commit0 get-tests command to retrieve test files
+    test_files_str = get_tests(repo_name, verbose=0)
+    test_files = sorted(list(set([i.split(":")[0] for i in test_files_str])))
+
+    # prepare the log dir
+    experiment_log_dir = (
+        Path(log_dir)
+        / repo_name
+        / branch
+        / datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    )
+    experiment_log_dir.mkdir(parents=True, exist_ok=True)
+
+    # write agent_config to .agent.yaml in the log_dir for record
+    agent_config_log_file = experiment_log_dir / ".agent.yaml"
+    with open(agent_config_log_file, "w") as agent_config_file:
+        yaml.dump(agent_config, agent_config_file)
+
+    # TODO: make this path more general
+    commit0_dot_file_path = str(Path(repo_path).parent.parent / ".commit0.yaml")
+    manager_message = "Write a concise plan of attack to implement the entire repo, but don't actually do any coding. The plan should not include any reccommendations to add files and should be a maximum of 500 words."
+    
+    with DirContext(repo_path):
+        if agent_config is None:
+            raise ValueError("Invalid input")
+        else:
+            # when unit test feedback is not available, iterate over target files to edit
+
+            update_queue.put(
+                ("start_repo", (original_repo_name, len(target_edit_files)))
+            )
+            
+            for f in target_edit_files:
+                update_queue.put(("set_current_file", (repo_name, f)))
+                dependencies = import_dependencies[f]
+            file_name = "all"
+            file_log_dir = experiment_log_dir / file_name
+            lint_cmd = get_lint_cmd(repo_name, agent_config.use_lint_info)
+                
+            
+            agent_return = manager.run(manager_message, "", lint_cmd, target_edit_files, file_log_dir)
+            with open(agent_return.log_file, 'r', encoding='utf-8') as file:
+                plan = file.read()
+            coder_message = "follow this implementation plan: "+plan
+
+            agent_return = coder.run(coder_message, "", lint_cmd, target_edit_files, file_log_dir)
+            
+            # for f in target_edit_files:
+            #     update_queue.put(("set_current_file", (repo_name, f)))
+            #     dependencies = import_dependencies[f]
+            #     message = update_message_with_dependencies(coder_message, dependencies)
+            #     file_name = f.replace(".py", "").replace("/", "__")
+            #     file_log_dir = experiment_log_dir / file_name
+            #     lint_cmd = get_lint_cmd(repo_name, agent_config.use_lint_info)
+            #     agent_return = coder.run(message, "", lint_cmd, [f], file_log_dir)
+            #     update_queue.put(
+            #         (
+            #             "update_money_display",
+            #             (repo_name, file_name, agent_return.last_cost),
+            #         )
+            #     )
+            update_queue.put(
+                    (
+                        "update_money_display",
+                        (repo_name, file_name, agent_return.last_cost),
+                    )
+                )
+    
+    
+    
     update_queue.put(("finish_repo", original_repo_name))
 
 
@@ -248,6 +410,7 @@ def run_agent(
                     result = pool.apply_async(
                         run_agent_for_repo,
                         args=(
+                            commit0_config,
                             commit0_config["base_dir"],
                             agent_config,
                             cast(RepoInstance, example),
@@ -256,6 +419,7 @@ def run_agent(
                             override_previous_changes,
                             backend,
                             log_dir,
+                            
                         ),
                     )
                     results.append(result)
