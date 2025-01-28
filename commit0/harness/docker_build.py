e3cfa8cccd75a166b7c5743295417f52778d8ad5
Justin Chiu <justinchiu@cohere.com>
2025-01-28 02:29:56 +0000

    fix docker build

--- commit0/harness/docker_build.py
+++ commit0/harness/docker_build.py
@@ -125,7 +125,7 @@ def build_base_images(
 
     """
     # Get the base images to build from the dataset
-    test_specs = get_specs_from_dataset(dataset, dataset_type)
+    test_specs = get_specs_from_dataset(dataset, dataset_type, absolute=True)
     base_images = {
         x.base_image_key: (x.base_dockerfile, x.platform) for x in test_specs
     }
